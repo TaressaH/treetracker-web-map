ccb6e7c8c2c0d3d05177b6e5048bbacd670c3097
Taressa Halpin <taressa.halpin@gmail.com>
2018-08-28 19:25:19 -0400

    Update map.js

--- js/map.js
+++ js/map.js
@@ -274,3 +274,31 @@ var initialize = function () {
 
 
 google.maps.event.addDomListener(window, 'load', initialize);
+
+//asynch: load script --feel free to change, critique
+//this function will work cross-browser for loading scripts asynchronously
+function loadScript(src, callback)
+{
+  var s,
+      r,
+      t;
+  r = false;
+  s = document.createElement('script');
+  s.type = 'text/javascript';
+  s.src = src;
+  s.onload = s.onreadystatechange = function() {
+    //console.log( this.readyState ); //uncomment this line to see which ready states are called.
+    if ( !r && (!this.readyState || this.readyState == 'complete') )
+    {
+      r = true;
+      callback();
+    }
+  };
+  t = document.getElementsByTagName('script')[0];
+  t.parentNode.insertBefore(s, t);
+}
+
+
+
+
+
